21e56b383abc307cf086e0501d78bd7434215b6c
chumxo33 <phinguyenanh@gmail.com>
2022-11-22 17:04:35 +0700

    Web_App

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([ 'prefix' => 'auth' ], function(){
+    Route::post('/login', [AuthController::class,'login'])->name('login');
+    Route::post('/register', [AuthController::class,'register'])->name('register');
+
+    Route::group(['middleware'=>'auth:api'], function(){
+        Route::delete('logout', [AuthController::class,'logout'])->name('logout');
+        Route::get('user', [AuthController::class,'user'])->name('user');
+        Route::get('/tin-da-doc',[Home_New::class,'recentViewed'])->name('recentViewed');
+
+    });
+});
+
+
